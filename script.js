ab032ba2ef0a1bc5b5dc00e7d08e742e7f8a9192
huan-chin chen <cherry032826@gmail.com>
2023-10-17 23:51:40 +0800

    new website, version 1.0

---
+++ script.js
@@ -0,0 +1,25 @@
+//open sidebar
+function showSidebar() {
+  let sidebar = document.querySelector(".sidebar");
+  sidebar.style.width = "50%";
+}
+
+//close sidebar
+function closeSidebar() {
+  let sidebar = document.querySelector(".sidebar");
+  sidebar.style.width = "0px";
+}
+
+//header background turn white when scrolling
+let header = document.querySelector("header");
+console.log(header);
+console.log(header.style);
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset != 0) {
+    header.style.backgroundColor = "rgba(255, 255, 255, 0.5)";
+    header.style.backdropFilter = "blur(1.7px)";
+  } else {
+    header.style.backgroundColor = "";
+    header.style.backdropFilter = "";
+  }
+});
